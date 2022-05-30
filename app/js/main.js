3bccf3ab1b0b913974e264a1af73e22d48516ebc
brouqboy <andrew.sotnikov1437@gmail.com>
2022-05-30 19:01:00 +0300

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,13 @@ $(function(){
     autoplay: true,
     autoplaySpeed: 3000
   })
+
+  $(".star").rateYo({
+    rating: 4,
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true
+  });
+
 })
